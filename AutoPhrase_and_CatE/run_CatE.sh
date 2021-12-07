860c00a99e1b734a139c5aa95c62245f760e2077
haowei <haowei98@uw.edu>
2021-12-06 17:46:29 -0800

    add AutoPhrase script result

--- AutoPhrase_and_CatE/run_CatE.sh
+++ AutoPhrase_and_CatE/run_CatE.sh
@@ -7,6 +7,6 @@ cd ..
 	-spec ./datasets/covid19/emb_topics_spec.txt \
 	-word-emb ./datasets/covid19/emb_topics_w.txt \
 	-topic-emb ./datasets/covid19/emb_topics_t.txt \
-	-res ./datasets/covid19/res_topics.txt -k 20 -binary 0 \
+	-res ./datasets/covid19/res_topics.txt -k 50 -binary 0 \
 	-size 100 -iter 6 -pretrain 2 -expand 1 -window 5 -negative 5 -sample 1e-3 -min-count 3 -threads 12
 	 
