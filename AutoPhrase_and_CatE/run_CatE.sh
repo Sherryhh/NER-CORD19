ad6059bd37dc7815f8e514f2e35e99b9e996049e
haowei <haowei98@uw.edu>
2021-12-06 17:03:21 -0800

    add CatE script

---
+++ AutoPhrase_and_CatE/run_CatE.sh
@@ -0,0 +1,12 @@
+cd src
+make cate
+cd ..
+
+./src/cate -train ./datasets/covid19/input_8k.txt -topic-name ./datasets/covid19/topics.txt \
+	-load-emb word2vec_100.txt \
+	-spec ./datasets/covid19/emb_topics_spec.txt \
+	-word-emb ./datasets/covid19/emb_topics_w.txt \
+	-topic-emb ./datasets/covid19/emb_topics_t.txt \
+	-res ./datasets/covid19/res_topics.txt -k 20 -binary 0 \
+	-size 100 -iter 6 -pretrain 2 -expand 1 -window 5 -negative 5 -sample 1e-3 -min-count 3 -threads 12
+	 
